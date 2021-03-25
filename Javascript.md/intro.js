838393a4e4130e43787187a9e3797a5a45a50534
Gaurav Patil <gauravpatil97886@gmail.com>
2021-03-25 22:09:08 +0530

    Update and rename Javascript to Javascript.md/intro.js

---
+++ Javascript.md/intro.js
@@ -0,0 +1,7 @@
+JavaScript is the world's most popular programming language.
+
+JavaScript is the programming language of the Web.
+
+JavaScript is easy to learn.
+
+This tutorial will teach you JavaScript from basic to advanced.
